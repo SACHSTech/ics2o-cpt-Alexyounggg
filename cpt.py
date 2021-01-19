93d5a27457b62a78f423aef4f465703d518fa472
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-01-19 15:50:48 +0000

    jan 19

--- cpt.py
+++ cpt.py
@@ -3,6 +3,7 @@ import random
 total = 0
 count = 0
 quiz = True
+game = True
 item_one = True
 item_two = True
 item_three = True
@@ -11,16 +12,19 @@ item_five = True
 
 print("*** Welcome to the Computer Buyer Game ***")
 
-money_claim = input("Enter 'money claim': ")
+# Enter money claim to start the game
+money_claim = input("Enter 'money claim': ").lower()
 
+#Loop the code until user enters money claim
 while money_claim != "money claim":
-  money_claim = input("Please type money claim. Watch for capitals: ")
+  money_claim = input("Please type money claim: ").lower()
 
 if money_claim == "money claim":
   total += 500
   print("You just claimed 500 dollars")
 
-while True:
+# Main program loop
+while game:
   action = input("What would you like to do next?( Instruction, risk, shop, \nmoney check, money earn): ")
 
   if action == "instruction":
@@ -30,47 +34,62 @@ while True:
     
 
   elif action == "risk":
-      risk_money = int(input("How much do you want to risk? (Enter integer): "))
-      while risk_money > total or risk_money <= 0:
-        risk_money = int(input("That is too much/too little money.\nYour balance is " +str(total)+ "\nHow much do you want to risk?(Enter integer): "))
-      if risk_money <= total:
-        fifty_chance = random.randint(0,1)
-        if fifty_chance == 0:
-            total += risk_money
-            print("You have won. Your total is", total)
-        elif fifty_chance == 1:
-            total -= risk_money
-            print("You have lost.Your total is", total)
+    while True:
+      try:
+        risk_money = int(input("How much do you want to risk? Your current balance is " +str(total) + "(Enterinteger): "))
+        while risk_money > total or risk_money <= 0:
+          risk_money = int(input("That is too much/too little money.\nYour balance is " +str(total)+ "\nHow much do you want to risk?(Enter integer): "))
+      except ValueError:
+        print("Please enter a number")
+        continue
+      else:
+        break
+      
+    if risk_money <= total:
+      fifty_chance = random.randint(0,1)
+      if fifty_chance == 0:
+          total += risk_money
+          print("You have won. Your balance is", total)
+      elif fifty_chance == 1:
+          total -= risk_money
+          print("You have lost.Your balance is", total)
 
   elif action == "shop":
-    shop_items = int(input("Welcome to the shop! Please pick a an item.\n 1. Gaming Mouse: $50 \n 2. Gaming Keyboard: $150 \n 3. I7 CPU: $300 \n 4. 3080 GPU: $700 \n 5. 144hrz monitor: $350 \n 6. Put the number here: "))
-    while shop_items >= 6:
-      shop_items = int(input("Please pick a number between 1-5: "))
+    while True:
+      try: 
+        shop_items = int(input("Welcome to the shop! Please pick a an item.\n 1. Gaming Mouse: $50 \n 2. Gaming Keyboard: $150 \n 3. I7 CPU: $300 \n 4. 3080 GPU: $700 \n 5. 144hrz monitor: $350 \n 6. Put the number here: "))
+        while shop_items >= 6:
+          shop_items = int(input("Please pick a number between 1-5: "))
+      except ValueError:
+        print("Please type a number")
+        continue
+      else:
+        break
     
     if shop_items == 1 and total >= 50 and item_one == True:
       item_one = False 
       total -= 50
-      print("Your new total is " +str(total)+ ". You have bought item one!")
+      print("Your new balance is " +str(total)+ ". You have bought item one!")
     
     elif shop_items == 2 and total >= 150 and item_two == True:
       item_two = False
       total -= 150
-      print("Your new total is " +str(total)+ ". You have bought item two!")
+      print("Your new balance is " +str(total)+ ". You have bought item two!")
 
     elif shop_items == 3 and total >= 300 and item_three == True:
       item_three = False
       total -= 300
-      print("Your new total is " +str(total)+ ". You have bought item three")
+      print("Your new balance is " +str(total)+ ". You have bought item three!")
 
     elif shop_items == 4 and total >= 700 and item_four == True:
       item_four = False
       total -= 700
-      print("Your new total is " +str(total)+ ". You have bought item four")
+      print("Your new balance is " +str(total)+ ". You have bought item four!")
 
     elif shop_items == 5 and total >= 350 and item_five == True:
       item_five = False
       total -= 350
-      print("Your new total is " +str(total)+ ". You have bought item five")
+      print("Your new balance is " +str(total)+ ". You have bought item five!")
 
     elif item_one == False and shop_items == 1 or item_two == False and shop_items == 2 or item_three== False and shop_items == 3 or item_four == False and shop_items == 4 or item_five == False and shop_items == 5:
       print("You have bought this item already")
@@ -93,40 +112,40 @@ while True:
     question_five = input("What is part of the networking program?\nA. Fan B. Lights C. Modem D. Book : ")
     
 
-    if question_one == "D":
+    if question_one == "D".lower():
       count += 1 
 
-    if question_two == "A":
+    if question_two == "A".lower():
       count += 1
 
-    if question_three == "A":
+    if question_three == "A".lower():
       count += 1
 
-    if question_four == "B":
+    if question_four == "B".lower():
       count += 1
 
-    if question_five == "C":
+    if question_five == "C".lower():
       count += 1
     
+    #Multiply to find the amount of money earned 
     money_earn = count * 100
     total += money_earn
     
-    print("You have earned " +str(money_earn)+ ". Your new total is " +str(total)+ ".")
+    print("You got " +str(count)+ " out of 5. You have earned " +str(money_earn)+ ". Your new total is " +str(total)+ ".")
 
-
-
-
-  elif action == "game check":
-    if item_one and item_two and item_three and item_four and item_five == False:
-      print("Congratulations. You have won the game.")
-    elif total == 0:
-      print("You have lost the game. Restart program to play again.")
-    else:
-      print("You have neither won or loss the game. Keep playing the game")
-
+  
   else: 
     print("Please enter instruction, risk, shop, or money check")
 
+  # Break the program if all items are bought 
+  if total > 0 and item_one == False and item_two == False and item_three == False and item_four == False and item_five == False:
+      game = False
+      print("Congratulations. You have won the game!")
+  
+  # Break the program when your total reaches zero
+  if total == 0:
+      game = False 
+      print("You have lost the game. Restart program to play again.")
 
 
 
